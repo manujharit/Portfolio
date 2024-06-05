d0ae5e15e00bec5f4702066d0f0fbae2cdbe2b12
Manuj Haritwal <123856937+manujharit@users.noreply.github.com>
2024-06-05 18:52:23 +0530

    Revert "new ui"

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,8 @@
 
+
 const Skills = ({ skill }) => {
     return (
-        <label className="mr-2 px-2 my-2 py-1 text-xs bg-cyan-800 rounded-3xl font-bold text-white">{skill}</label>
+        <label className="mx-2 px-2 my-2 py-1 text-xs bg-gradient-to-r from-red-500 to-orange-500 rounded-3xl font-semibold text-white">{skill}</label>
     )
 }
 
