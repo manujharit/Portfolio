753463b026b5b252132b646649d32d21da0ee921
Manuj Haritwal <manuj.haritwal01@gmail.com>
2024-06-05 19:39:57 +0530

    V3-new

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,6 +16,9 @@ const Navbar = () => {
         window.scroll(0, 0)
     };
 
+    const handleLink = () => {
+        window.scrollTo(0, 0)
+    }
     useEffect(() => {
         if (!isOpen) {
             window.scrollTo(0, 0)
@@ -25,10 +28,10 @@ const Navbar = () => {
     return (
         <div className='z-10 fixed top-0 flex items-center justify-between bg-opacity-50 bg-white/50 backdrop-blur-sm border lg:my-[1%] px-[5%] w-[100%] md:w-[85%] lg:w-[80%] xl:w-[50%] md:rounded-2xl lg:rounded-2xl h-[7%] drop-shadow-md shadow-gray-700'>
             <div className='flex justify-center w-[50%] md:w-[40%] lg:w-[40%] '>
-                <Link to='/' className='font-extrabold text-[150%] text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500'>{"</> MANUJ"}</Link>
+                <Link to='/' className='font-extrabold text-[150%] text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500'  onClick={handleLink}>{"</> MANUJ"}</Link>
             </div>
             <div className='hidden md:flex justify-between w-[45%]'>
-                {Links.map(link => <Link key={link.name} to={link.link} className='text-black hover:text-violet-500 font-semibold'>{link.name}</Link>)}
+                {Links.map(link => <Link key={link.name} to={link.link} className='text-black hover:text-violet-500 font-semibold' onClick={handleLink}>{link.name}</Link>)}
             </div>
             <div className='md:hidden flex items-center mr-5'>
                 <button onClick={() => setIsOpen(!isOpen)} className='text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 font-bold '>
